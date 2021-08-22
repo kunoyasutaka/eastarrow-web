a911e6fa309ba64581950262ca472f2936900a8c
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-08-23 01:45:30 +0900

    ログイン処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase/firebase.dart' as firebase;
 import 'config/admin_app.dart';
 
 Future main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   // ユーザのログイン状態が確定するまで待つ
   await Firebase.initializeApp();
-  //await firebase.auth(firebase.app()).onAuthStateChanged.first;
+  await firebase.auth(firebase.app()).onAuthStateChanged.first;
 
   runApp(AdminApp());
 }
